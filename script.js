ef349eef4f1a56939938aa5d49051ab116a63524
Md Amran Hossen <mdamranhossen77@gmail.com>
2023-07-20 15:34:08 +0600

    complete js functionality

--- script.js
+++ script.js
@@ -1,44 +1,56 @@
-let currentValue = '';
+let currentValue = "";
 let operatorClicked = false;
+let lastOperator = "";
+let lastValue = "";
 
-
-function updateDisplay(){
-    const display = document.getElementById('display');
-    display.value = currentValue || '0';
+function updateDisplay() {
+  const display = document.getElementById("display");
+  display.value = currentValue || "0";
 }
 
-function appendValue(value){
-    if(operatorClicked){
-        currentValue = '';
-        operatorClicked = false;
-    }
+function appendValue(value) {
+  if (operatorClicked) {
+    currentValue = "";
+    operatorClicked = false;
+  }
 
-    currentValue += value;
-    updateDisplay();
+  currentValue += value;
+  updateDisplay();
 }
 
-
-function calculate(){
-    if(currentValue === '' || operatorClicked){
-        const result = eval(currentValue);
-        currentValue = result.toString();
-        updateDisplay();
-    }
+function calculate() {
+  if (currentValue === "" || operatorClicked) return;
+  currentValue = currentValue.replace(/÷/g, "/");
+  currentValue = currentValue.replace(/x/g, "*");
+  currentValue = currentValue.replace(/−/g, "-");
+  const result = eval(currentValue);
+  currentValue = result.toString();
+  updateDisplay();
+}
+function clearDisplay() {
+  currentValue = "";
+  updateDisplay();
 }
 
-function clearDisplay(){
-    currentValue = '';
-    updateDisplay();
+function deleteValue() {
+  currentValue = currentValue.slice(0, -1);
+  updateDisplay();
 }
 
-function deleteValue(){
-    currentValue = currentValue.slice(0, -1);
-    updateDisplay();
+function calculatePercentage() {
+  if (currentValue === "") return;
+  currentValue = (parseFloat(currentValue) / 100).toString();
+  updateDisplay();
 }
 
+function calculateSquare() {
+  if (currentValue === "") return;
+  currentValue = (parseFloat(currentValue) ** 2).toString();
+  updateDisplay();
+}
 
 updateDisplay();
 
 // function calculatePercentage(){
-    
+
 // }
